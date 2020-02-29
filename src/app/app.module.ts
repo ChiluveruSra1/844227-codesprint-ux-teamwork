3ff1f6aab034c8c485e9d4a714c4aaa5e56d5681
ChiluveruSra1 <Chiluveru123@gmail.com>
2020-02-29 17:04:45 +0530

    844227 teamwork

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { ViewOrderComponent } from './components/view-order/view-order.component';
+import { PlaceOrderComponent } from './components/place-order/place-order.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ContactUsComponent,
+    ViewOrderComponent,
+    PlaceOrderComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
